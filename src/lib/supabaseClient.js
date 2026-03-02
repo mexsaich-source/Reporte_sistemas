77ba8bf9da90fed91511c09ad2f03d93dd90bfb3
yoel11705 <mryexs117@gmail.com>
2026-03-02 09:10:49 -0600

    1.4.8

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,5 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Creamos una única instancia del cliente de Supabase para reutilizar en toda la app
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
